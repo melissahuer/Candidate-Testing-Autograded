e3ed65d39574c2bcbcf2e58c8a828d7ea7c38815
Melissa Huerta <melissahuerta@Mac-mini.local>
2024-06-20 21:07:07 -0500

    part 1 of assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -17,7 +17,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("Enter your name:");
+candidateName = input.question("Enter your name: ");
 
 }
 
